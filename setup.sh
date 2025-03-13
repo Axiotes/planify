a882bd6ccb7cc9593dfb3e24a9aeaf2d095e2e7a
Arthur Axiotes <axiotesarthur@gmail.com>
2025-03-12 21:58:17 -0300

    feat: adicionar config base do .env para banco de dados local

--- setup.sh
+++ setup.sh
@@ -1,15 +1,37 @@
-main() {
+cloneRepositories() {
     echo "Clonando repositório da api..."
     git clone git@github.com:Axiotes/api-planify.git
 
     echo "Clonando repositório do app..."
     git clone git@github.com:Axiotes/app-planify.git
+}
 
-    echo "Relizando o build da aplicação..."
-    docker-compose build
+configEnv() {
+    cd api-planify
 
-    echo "Iniciando a aplicação..."
-    docker-compose up
+    if [ ! -f ".env" ]; then
+        echo "Criando o arquivo .env..."
+
+        echo "HOST=db" > .env
+        echo "DATABASE=db_planify" >> .env
+        echo "USER_NAME=root" >> .env
+        echo "PASSWORD=root" >> .env
+        echo "PORT=3306" >> .env
+
+        echo ".env criado com as configurações padrão."
+    else
+        echo ".env já existe. Verificando configurações..."
+    fi
+
+    cd ..
+}
+
+main() {
+    cloneRepositories
+    configEnv
+
+    echo "Relizando o build e inicializando aplicação..."
+    docker-compose up --build
 }
 
 main
